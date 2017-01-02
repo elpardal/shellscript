6bee5dd9b5fd214f1c5bfde27a738af4d3eac1cb
Elismar Luz <elismar@elismarluz.com>
2017-01-02 11:22:43 -0200

    Create commands.sh
    script que é executado remotamente via execute.sh

---
+++ commands.sh
@@ -0,0 +1,33 @@
+HOST_MACADDRES=$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address)
+HOST_HOSTNAME=$(hostname)
+HOST_USERS=$(who -q | head -1)
+
+OCS_PACKAGE="ocsinventory-agent"
+OCS_SERVER="10.221.37.60"
+OCS_FILE="/etc/ocsinventory/ocsinventory-agent.cfg"
+
+function install_ocs(){
+  DEBIAN_FRONTEND=noninteractive apt-get -y install $OCS_PACKAGE
+  echo "server=$OCS_SERVER > $OCS_FILE"
+  ocsinventory-agent
+}
+function registrar_correcao(){
+  case $1 in
+    puppet)
+      echo "tag0002 - Demanda corretiva do puppet executada remotamente em: `date +%d/%m/%Y\ \à\s\ %H:%M:%S`" >> /var/log/stefanini.log
+      ;;
+    ocs)
+      echo "tag0003 - Demanda de instalacao do $OCS_PACKAGE executada remotamente em: `date +%d/%m/%Y\ \à\s\ %H:%M:%S`" >> /var/log/stefanini.log
+      ;;
+    *)
+      exit
+  esac
+}
+if [ -f /etc/ocsinventory/ocsinventory-agent.cfg ]
+  then
+    echo \"$HOST_MACADDRES\",\"$HOST_HOSTNAME\",\"$HOST_USERS\",\"1\"
+  else
+    echo \"$HOST_MACADDRES\",\"$HOST_HOSTNAME\",\"$HOST_USERS\",\"0\"
+    install_ocs
+    registrar_correcao ocs
+fi
