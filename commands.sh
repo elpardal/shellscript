aadeac415e7e4791ef21ba50a4e6c81fdb5440de
Elismar Luz <elismar@elismarluz.com>
2017-01-02 11:57:31 -0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -7,9 +7,9 @@ OCS_SERVER="10.221.37.60"
 OCS_FILE="/etc/ocsinventory/ocsinventory-agent.cfg"
 
 function install_ocs(){
-  DEBIAN_FRONTEND=noninteractive apt-get -y install $OCS_PACKAGE
+  DEBIAN_FRONTEND=noninteractive apt-get -y install $OCS_PACKAGE &> /dev/null
   echo "server=$OCS_SERVER > $OCS_FILE"
-  ocsinventory-agent
+  ocsinventory-agent &> /dev/null
 }
 function registrar_correcao(){
   case $1 in
@@ -26,6 +26,9 @@ function registrar_correcao(){
 if [ -f /etc/ocsinventory/ocsinventory-agent.cfg ]
   then
     echo \"$HOST_MACADDRES\",\"$HOST_HOSTNAME\",\"$HOST_USERS\",\"1\"
+    echo "$OCS_PACKAGE"
+    echo "OCS_FILE"
+    echo "OCS_SERVER"
   else
     echo \"$HOST_MACADDRES\",\"$HOST_HOSTNAME\",\"$HOST_USERS\",\"0\"
     install_ocs
